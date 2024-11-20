e4e82d4822b965e3dba7c8d14380d3c00b75f70c
winoooops <winoooops@outlook.com>
2024-11-20 11:43:23 +0800

    FIX: fix incorrect schma

--- server/src/schemas/message.schema.ts
+++ server/src/schemas/message.schema.ts
@@ -9,6 +9,6 @@ export const MessageSchema = `#graphql
   }
 
   type Mutation {
-    createTextReplyFromConversation(messages: [LunaiMessage]!): Message
+    createTextReplyFromConversation(messages: [LunaiMessage]!): LunaiMessage
   }
 `
